43b3cb50a980af9b5442787494d00c6397974b91
Ony Brunella Andriatsahavojaona <brunella.andria@gmail.com>
2024-05-28 23:26:37 +0200

    fix issue back to top; add preloader; added content-collapsible for soft skills

--- assets/js/back-to-top.js
+++ assets/js/back-to-top.js
@@ -1,21 +1,38 @@
   
-  // JavaScript pour le bouton "Haut de page"
+  // // JavaScript pour le bouton "Haut de page"
 
-  $(document).ready(function() {
-    // Montrer ou cacher le bouton "Haut de page" en fonction du défilement de la page
-    $(window).scroll(function() {
-      if ($(this).scrollTop() > 200) {
-        $('.back-to-top').fadeIn(200);
-      } else {
-        $('.back-to-top').fadeOut(200);
-      }
-    });
+  // $(document).ready(function() {
+  //   // Montrer ou cacher le bouton "Haut de page" en fonction du défilement de la page
+  //   $(window).scroll(function() {
+  //     if ($(this).scrollTop() > 200) {
+  //       $('.back-to-top').fadeIn(200);
+  //     } else {
+  //       $('.back-to-top').fadeOut(200);
+  //     }
+  //   });
     
-    // Animer le bouton "Haut de page" vers le haut de la page
-    $('.back-to-top').click(function(event) {
-      event.preventDefault();
+  //   // Animer le bouton "Haut de page" vers le haut de la page
+  //   $('.back-to-top').click(function(event) {
+  //     event.preventDefault();
       
-      $('html, body').animate({scrollTop: 0}, 300);
-    })
-  });
+  //     $('html, body').animate({scrollTop: 0}, 300);
+  //   })
+  // });
   
+
+  document.addEventListener("DOMContentLoaded", function () {
+  // Montrer ou cacher le bouton "Haut de page" en fonction du défilement de la page
+  window.addEventListener("scroll", function() {
+    if (window.scrollY > 200) {
+      document.querySelector('.back-to-top').style.display = 'block';
+    } else {
+      document.querySelector('.back-to-top').style.display = 'none';
+    }
+  });
+
+  // Animer le bouton "Haut de page" vers le haut de la page
+  document.querySelector('.back-to-top').addEventListener("click", function(event) {
+    event.preventDefault();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
+});
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,54 +1,109 @@
-// Animation effet machine à écrire sur "Permettez-moi de me présenter"
+// window.addEventListener("DOMContentLoaded", (event) => {
+//   repeatAnimation();
+// });
 
-window.addEventListener("DOMContentLoaded", (event) => {
+// function repeatAnimation() {
+//   let texte = document.querySelector('h2.text-animate');
+//   let originalContent = texte.innerHTML;
+//   texte.innerHTML = '';
+//   let index = 0;
+
+//   function typeWriter() {
+//     if (index < originalContent.length) {
+//       texte.innerHTML += originalContent.charAt(index);
+//       index++;
+//       setTimeout(typeWriter, 300);
+//     } else {
+//       setTimeout(repeatAnimation, 1000);
+//     }
+//   }
+
+//   typeWriter();
+// }
+
+// $(document).ready(function() {
+//   $("nav").find("li").on("click", "a", function() {
+//     $('.navbar-collapse.in').collapse('hide');
+//   });
+
+//   $(".navbar a, footer a[href='#home']").on('click', function(event) {
+//     if (this.hash !== "" && $(window).width() > 480) {
+//       event.preventDefault();
+//       var hash = this.hash;
+//       $('html, body').animate({
+//         scrollTop: $(hash).offset().top
+//       }, 700, function() {
+//         window.location.hash = hash;
+//       });
+//     }
+//   });
+
+// });
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     setTimeout(function() {
+//         var preloader = document.querySelector('.preloader');
+//         var content = document.getElementById('content');
+//         preloader.style.display = 'none';
+//         content.style.display = 'block';
+//         content.style.opacity = '1';
+//     }, 3000);
+// });
+
+
+document.addEventListener("DOMContentLoaded", function () {
   repeatAnimation();
+
+  $("nav").find("li").on("click", "a", function() {
+    $('.navbar-collapse.in').collapse('hide');
+  });
+
+  $(".navbar a, footer a[href='#home']").on('click', function(event) {
+    if (this.hash !== "" && $(window).width() > 480) {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 700, function() {
+        window.location.hash = hash;
+      });
+    }
+  });
+
+  function repeatAnimation() {
+    let texte = document.querySelector('h2.text-animate');
+    let originalContent = texte.innerHTML;
+    texte.innerHTML = '';
+    let index = 0;
+
+    function typeWriter() {
+      if (index < originalContent.length) {
+        texte.innerHTML += originalContent.charAt(index);
+        index++;
+        setTimeout(typeWriter, 300);
+      } else {
+        setTimeout(repeatAnimation, 1000);
+      }
+    }
+
+    typeWriter();
+  }
 });
 
-function repeatAnimation() {
-  let texte = document.querySelector('h2.text-animate');
-  let originalContent = texte.innerHTML;
-  texte.innerHTML = '';
-  let index = 0;
+document.addEventListener("DOMContentLoaded", function () {
 
-  function typeWriter() {
-    if (index < originalContent.length) {
-      texte.innerHTML += originalContent.charAt(index);
-      index++;
-      setTimeout(typeWriter, 300); 
-    } else {
-      setTimeout(repeatAnimation, 1000); 
-    }
-  }
-
-  typeWriter(); 
-}
+  document.body.classList.add('no-scroll');
   
-  // JavaScript (Jqeury) pour le menu hamburger avec un effet de défilement en douceur
-  
-  $(document).ready(function() {   // Activer la fonction qui suit lorsque la page est chargée
-    // Correction pour que le menu hamburger se replie après un clic sur un élément de menu.
-    $("nav").find("li").on("click", "a", function() {
-      $('.navbar-collapse.in').collapse('hide');
-    });
+  window.scrollTo(0, 0);
 
-    // Ajouter un défilement en douceur à tous les liens de navigation.
-    $(".navbar a, footer a[href='#home']").on('click', function(event) {
-      // Ne pas faire défiler si l'on est sur un portable
-      if (this.hash !== "" && $(window).width() > 480) {
-        // Empêcher le comportement de clic d'ancre par défaut.
-        event.preventDefault();
-        // Stocker la variable de hachage.
-        var hash = this.hash;
-        // Faites défiler jusqu'à la balise de hachage en 700 ms.
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 700, function() {
-        // Ajouter le hachage (#) à l'URL une fois le défilement terminé.
-          window.location.hash = hash;
-        });
-      }
-    });
+  setTimeout(function() {
+    var loader = document.querySelector('.loader');
+    var loaderContent = document.querySelector('.loader-content');
+    var content = document.getElementById('content');
+    loader.style.opacity = '0';
+    loaderContent.style.opacity = '0';
+    content.style.opacity = '1';
 
-  }); 
-
-  
+    document.body.classList.remove('no-scroll');
+  }, 4000);
+});
